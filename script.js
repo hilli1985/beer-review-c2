5f559e2931266db2bcd1878b2d49542f169d56c4
Hilla Meir <hillameir85@gmail.com>
2018-08-01 11:40:00 +0300

    update - extension added

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 const beers = [];
+let sortLogic = true;
 
-function addBeer(name, category) {
-    let beer = { name: name, category: category };
+function addBeer(name, category, rate) {
+    let beer = { name: name, category: category, rate: rate };
     beers.push(beer);
     console.log(beers);
 }
 
 $('.post-beer').on('click', function() {
-    addBeer($(".beer-input").val(), $(".category-input").val());
+    addBeer($(".beer-input").val(), $(".category-input").val(), $(".rate-input option:selected").text());
+    renderBeers();
+});
+$('.sort-beer').on('click', function() {
+    if (sortLogic) {
+        beers.sort(function(a, b) { return (a.rate - b.rate) });
+        sortLogic = false;
+    } else {
+        beers.sort(function(a, b) { return (b.rate - a.rate) });
+        sortLogic = true;
+    }
     renderBeers();
 });
 
 function renderBeers() {
-    // alert('render');
+    let rate = "";
     $(".beers-list").find('li').remove();
     for (let beer of beers) {
-        $(".beers-list").append($("<li>").text(beer.name));
+        if (parseInt(beer.rate) >= 0) {
+            rate = " , " + beer.rate + "-stars";
+        }
+        $(".beers-list").append($("<li>").text(beer.name + " , " + beer.category + rate));
     }
 }
